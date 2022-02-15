4c5f40dec94388abf56646dc7762670fdcb8c5b6
kirillbushmanov <dissin101@gmail.com>
2022-02-15 20:07:05 +0600

    Add Bootstrap grid, normalize.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import './assets/styles/bootstrap-grid.css';
+import './assets/styles/normalize.css';
+import './App.scss';
 
 const App = () => {
-  return (
-      <div>
-
-      </div>
-  );
+    return (
+        <div className={'test'}>
+            <div className={'container'}>
+                TEST
+            </div>
+        </div>
+    );
 };
 
 export default App;
