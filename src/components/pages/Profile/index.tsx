b0b5608017cf39519b6b26d4c2caf9d57c75934e
kirillbushmanov <dissin101@gmail.com>
2022-05-28 19:17:21 +0600

    feature/webappclient-4 Add profile route

--- src/components/pages/Profile/index.tsx
+++ src/components/pages/Profile/index.tsx
@@ -1,10 +1,59 @@
-import React from 'react';
+import React, {useState} from 'react';
+import styles from './Profile.module.scss';
+import Breadcrumbs from "../../UI/Breadcrumbs";
+import {useLocation, useNavigate} from "react-router-dom";
 
 const Profile = () => {
+
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    const [breadcrumbs, setBreadcrumbs] = useState([
+        {title: 'Главная', path: '#'},
+        {title: 'Профиль', path: "/profile"}
+    ]);
+
+    const navigationLinks = [
+        {
+            name: "settings",
+            path: "/profile?module=settings",
+            title: "Настройки"
+        },
+        {
+            name: "order-history",
+            path: "/profile?module=order-history",
+            title: "История заказов"
+        },
+        {
+            name: "product-add",
+            path: "/profile?module=product-add",
+            title: "Добавление товара"
+        }
+    ];
+
     return (
-        <div>
-            Profile
-        </div>
+        <>
+            <Breadcrumbs links={breadcrumbs}/>
+
+            <div className={'box m-t-8'}>
+                <div className={'row'}>
+                    <div className={'col-4'}>
+                        <h3 className={'m-t-0 m-b-8'}>Навигация</h3>
+                        <ul className={styles['navigation']}>
+                            {navigationLinks.map(({name, path, title}) => {
+                                return(
+                                    <li className={styles['navigation__item']}>
+                                        <span onClick={() => navigate(path)}>{title}</span>
+                                    </li>
+                                )
+                            })}
+                        </ul>
+                    </div>
+                    <div className={'col-8'}>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 };
 
